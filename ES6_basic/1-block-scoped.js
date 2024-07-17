e1728dc280fbd332d88837fbf7cafc82533dddc3
nihad1213 <nihad.nemetli@gmail.com>
2024-07-17 22:19:30 +0400

    Task 0

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    var task = false;
+    var task2 = true;
   
     if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
+      var taskInner = true;
+      var task2Inner = false;
     }
   
     return [task, task2];
