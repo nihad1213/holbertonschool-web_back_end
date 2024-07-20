79619cc9c60fa18da57b0f9ece3a5fd270fcba9b
nihad1213 <nihad.nemetli@gmail.com>
2024-07-20 12:38:22 +0400

    Task

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,27 +1,27 @@
 // 3-currency.js
 export default class Currency {
     constructor(code, name) {
-        this._code = code;
-        this._name = name;
+      this._code = code;
+      this._name = name;
     }
-
+  
     displayFullCurrency() {
-        return `${this._name} (${this._code})`;
+      return `${this._name} (${this._code})`;
     }
-
+  
     get code() {
-        return this._code;
+      return this._code;
     }
-
+  
     set code(newCode) {
-        this._code = newCode;
+      this._code = newCode;
     }
-
+  
     get name() {
-        return this._name;
+      return this._name;
     }
-
+  
     set name(newName) {
-        this._name = newName;
+      this._name = newName;
     }
 }
