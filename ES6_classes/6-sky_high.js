81189fbe8c357e0667ee8cfa923ba69654681abc
nihad1213 <nihad.nemetli@gmail.com>
2024-07-20 12:49:14 +0400

    Task

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+/* eslint-disable */
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
