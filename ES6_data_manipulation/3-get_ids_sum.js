c268ff9397ce54d1556fb5609ee60b314edb8c21
nihad1213 <nihad.nemetli@gmail.com>
2024-07-24 13:19:57 +0400

    Task 3

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default function getStudentIdsSum(students) {
     if (Object.getPrototypeOf(students) === Array.prototype) {
-        const ids = stduents.map((ids) => ids.id);
-        const reducer = (accumlator, currentValue) => accumlator + currentValue;
-        return ids.reduce(reducer);
+        const ids = students.map((student) => student.id);
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return ids.reduce(reducer, 0);
     }
 
-    return [];
+    return 0;
 }
