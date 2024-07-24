b6888d122f0f3cc7ebef8971c748ac791dd07a1d
nihad1213 <nihad.nemetli@gmail.com>
2024-07-24 14:04:00 +0400

    Task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,20 @@
 /* eslint-disable */
 export default function updateStudentGradeByCity(arrOfStudentObjs, city, newGrades) {
-    if (Array.isArray(arrOfStudentObjs) && typeof city === 'string' && typeof newGrades === 'object') {
-      const filtStudents = arrOfStudentObjs.filter((obj) => obj.location === city);
-  
-      filtStudents.forEach((studentObj) => {
-        const objFound = newGrades.find((obj) => obj.studentId === studentObj.id);
-        if (objFound) { studentObj.grade = objFound.grade; } else { studentObj.grade = 'N/A'; }
-      });
-  
-      return filtStudents;
-    }
-    return [];
+  if (Array.isArray(arrOfStudentObjs) && typeof city === 'string' && Array.isArray(newGrades)) {
+    // Filter students based on city
+    const filteredStudents = arrOfStudentObjs.filter(student => student.location === city);
+
+    // Map through filtered students and update grades
+    const updatedStudents = filteredStudents.map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      if (gradeObj) {
+        return { ...student, grade: gradeObj.grade };
+      } else {
+        return { ...student, grade: 'N/A' };
+      }
+    });
+
+    return updatedStudents;
   }
+  return [];
+}
