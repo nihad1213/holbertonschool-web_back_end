e8fff04dbfcc153549e0bd9a3cfc7d3027857053
nihad1213 <nihad.nemetli@gmail.com>
2024-07-26 11:05:32 +0400

    Task 5

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
-export default function uploadPhoto($filename) {
-    return Promise.reject(new Error('${$filename} cannot be processed'));
+export default function uploadPhoto(filename) {
+    return Promise.reject(new Error(`${filename} cannot be processed`));
 }
