91622280c1becd3768fdeee00b398a4c77b4288b
nihad1213 <nihad.nemetli@gmail.com>
2024-07-26 10:48:53 +0400

    Task 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export function getFullResponseFromAPI(success = true) {
+export function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
         if (success) {
             resolve({
