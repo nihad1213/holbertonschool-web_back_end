d44d759564f22553dd7c0235328422caab2edaae
nihad1213 <nihad.nemetli@gmail.com>
2024-07-26 10:49:32 +0400

    Task 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
-export function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success',
-            });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
+export default function getFullResponseFromAPI(success) {
+    const myPromise = new Promise((resolve, reject) => {
+      if (success) {
+        resolve(
+          { status: 200, body: 'Success' },
+        );
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
     });
-}
+    return myPromise;
+  }
