a186089929c4682ee8b60076704c206980b09278
nihad1213 <nihad.nemetli@gmail.com>
2024-07-26 11:01:43 +0400

    Task 4

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function signUpUser(firstName, lastName) {
+    const myPromise = new Promise((resolve) => {
+      resolve(
+        { firstName, lastName },
+      );
+    });
+    return myPromise;
+  }
