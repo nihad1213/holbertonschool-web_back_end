c884bd21e62f936f79c5783d172f01f9c5ac8139
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-07-26 11:41:42 +0400

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,13 +3,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName)
-    ]).then((results) => (
-        results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason
-        }))
-    ));
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+
+  const fileData = await uploadPhoto(fileName).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
+
+  return Promise.resolve([userData, fileData]);
 }
