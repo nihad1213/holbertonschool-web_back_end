418f469960541a1f3f20d31a891501afeaa980b4
nihad1213 <nihad.nemetli@gmail.com>
2024-07-26 11:20:22 +0400

    Task 9

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function guardrail(mathFunction) {
+    const array = [];
+  
+    try {
+      array.push(mathFunction());
+    } catch (err) {
+      array.push(err.toString());
+    } finally {
+      array.push('Guardrail was processed');
+    }
+    return array;
+  }
