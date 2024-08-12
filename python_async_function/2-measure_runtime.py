ccf27d1e312ba3936b3fdea1b534a32e24ac9ba9
nihad1213 <nihad.nemetli@gmail.com>
2024-08-13 01:28:59 +0400

    Task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Python"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Return formula"""
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+    return (end_time - start_time) / n
