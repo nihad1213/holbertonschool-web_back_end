87947e9c32ba5936dbd70d2a90fe1efd4b967cf9
nihad1213 <nihad.nemetli@gmail.com>
2024-08-13 01:09:18 +0400

    Task 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""Geceleer"""
+"""SAGOPA KJMER"""
 import asyncio
 import random
 
-
 async def wait_random(max_delay: int = 10) -> float:
-    """return randim_numbir"""
-    random_number = random.uniform(0, max_delay)
-    await asyncio.sleep(random_number)
-    return random_number
+    """What a function"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
