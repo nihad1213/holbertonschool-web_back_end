2fef595cb6895bfa0df3ef8f9f6f943774c325e8
nihad1213 <nihad.nemetli@gmail.com>
2024-08-13 10:30:56 +0400

    Task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-"""Python"""
+"""Module for creating and managing asynchronous tasks."""
 from typing import List
 import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Test"""
-    futures = [task_wait_random(max_delay) for _ in range(n)]
-    futures = asyncio.as_completed(futures)
-    delays = [await future for future in futures]
-    return delays
+    """Create and run n tasks that each wait for a random delay.
 
+    Args:
+        n (int): The number of tasks to create.
+        max_delay (int): The maximum delay for each task.
+
+    Returns:
+        List[float]: A list of delays corresponding to each task.
+    """
+    futures = [task_wait_random(max_delay) for _ in range(n)]
+    delays = [await future for future in asyncio.as_completed(futures)]
+    return delays
