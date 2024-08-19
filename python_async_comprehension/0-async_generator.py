036bae6360284e96e9c2cb10947a4ac3f7080b1e
nihad1213 <nihad.nemetli@gmail.com>
2024-08-19 12:18:50 +0400

    Tasks

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Module for asynchronous generator."""
+"""A"""
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Function for asynchronous generator."""
+    """B"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
